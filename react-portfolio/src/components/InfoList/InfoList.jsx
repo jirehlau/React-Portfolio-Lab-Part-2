28370110859bc45395e563c511c20f79da930152
Jireh Lau <jirehcylau@gmail.com>
2021-04-13 15:52:37 -0400

    tasks 1

---
+++ react-portfolio/src/components/InfoList/InfoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function InfoList(props) {
+    return(
+        <div className='InfoListComponent'>
+            {/* <h2>InfoList</h2> */}
+            <p>InfoListItem</p>
+            <p>InfoListItem</p>
+            <p>InfoListItem</p>
+        </div>
+        
+    );
+}
+
+export default InfoList;
